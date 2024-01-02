a5016187dfa479ca99f9dc071e535e39722b707e
guosoun <2236998782@qq.com>
2024-01-02 21:16:41 +0800

    自动获取分值

--- modules/problem.js
+++ modules/problem.js
@@ -359,7 +359,7 @@ app.get('/problem/:id/export', async (req, res) => {
     if(!res.locals.user || !res.locals.user.is_admin) throw new ErrorMessage('您没有权限进行此操作。');
     let id = parseInt(req.params.id);
     let problem = await Problem.findById(id);
-    if (!problem || !problem.is_public) throw new ErrorMessage('无此题目。');
+    if (!problem) throw new ErrorMessage('无此题目。');
 
     let obj = {
       title: problem.title,
