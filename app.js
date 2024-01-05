82b3b386d95bc1c3cf806dda9a4595170589a8ea
guosoun <2236998782@qq.com>
2024-01-05 21:23:35 +0800

    修

--- app.js
+++ app.js
@@ -353,7 +353,7 @@ global.syzoj = {
     app.use((req, res, next) => {
       res.locals.req = req;
       res.locals.res = res;
-      if (!user) next();
+      if (user === undefined) next();
       res.locals.user.getPrivileges()
       .then(privileges => {
         res.locals.user.privileges = privileges;
